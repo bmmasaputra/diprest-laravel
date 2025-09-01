875c89b3ca62509658a6b159430fe16536bc87ab
Rimiruuu21 <arbylimingchristianarby@gmail.com>
2025-09-01 19:35:57 +0700

    feat: Add DataPrestasi resource with CRUD functionality and form schema

---
+++ app/Filament/Resources/DataPrestasis/DataPrestasiResource.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Resources\DataPrestasis;
+
+use App\Filament\Resources\DataPrestasis\Pages\CreateDataPrestasi;
+use App\Filament\Resources\DataPrestasis\Pages\EditDataPrestasi;
+use App\Filament\Resources\DataPrestasis\Pages\ListDataPrestasis;
+use App\Filament\Resources\DataPrestasis\Schemas\DataPrestasiForm;
+use App\Filament\Resources\DataPrestasis\Tables\DataPrestasisTable;
+use App\Models\DataPrestasi;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+
+class DataPrestasiResource extends Resource
+{
+    protected static ?string $model = DataPrestasi::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static ?string $recordTitleAttribute = 'nim';
+
+    public static function form(Schema $schema): Schema
+    {
+        return DataPrestasiForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return DataPrestasisTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListDataPrestasis::route('/'),
+            'create' => CreateDataPrestasi::route('/create'),
+            'edit' => EditDataPrestasi::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/DataPrestasis/Pages/CreateDataPrestasi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\DataPrestasis\Pages;
+
+use App\Filament\Resources\DataPrestasis\DataPrestasiResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateDataPrestasi extends CreateRecord
+{
+    protected static string $resource = DataPrestasiResource::class;
+}
